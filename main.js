879e4ed0278ac2cb1263d825e835ed8b591dc2be
tjcutt <TJCutt@Timothys-MacBook-Pro.local>
2016-10-27 11:22:31 -0600

    Updates
    Updated version after James helped me to remove HTML printing glitch

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-/*
+/*========================COMPLETED
+ONE FORMATTING QUESTION AT THE END
 =======================================================
 ** Week 1 - Project 2 **
 
@@ -9,34 +10,22 @@ problem.
 =======================================================
 */
 
-
 // 1. Connect the main.js document to the HTML page.
 //    Then Update the Text to Green by adding the .status-good class.
-
-
 
 // When the JS is connected properly, the following code will update the status message.
 document.getElementById("q1").innerHTML = "JS Page Connected Properly!";
 document.getElementById("q1").classList.add("status-good");
-
-
 
 // 2. Declare a new variable called 'feelings',
 //    Then store a string with a word that describes
 //    how well you feel about this course material.
 //    Then display that string within the HTML page.
 
-
 var feelings;
 feelings = "Excited but feeling overwhelmed at times";
 
 document.getElementById("q2").innerHTML = feelings;
-
-
-
-
-
-
 
 // 3. Declare a new variable called 'name',
 //    store your name within it, and then
@@ -46,14 +35,9 @@ document.getElementById("q2").innerHTML = feelings;
 
 var name = " Tim";
 var message = "Hello, my name is";
-var sentence =  message + name
+var sentence = message + name
 
 document.getElementById("q3").innerHTML = sentence;
-
-
-
-
-
 
 // 4. Declare two variables with number values.
 //    Add, subtract, multiply and divide them.
@@ -65,15 +49,12 @@ var n2 = 6
 var add = n1 + n2
 var sub = n1 - n2
 var mult = n1 * n2
-var div = n1/n2
+var div = n1 / n2
 
 document.getElementById("q4a").innerHTML = add
 document.getElementById("q4b").innerHTML = sub
 document.getElementById("q4c").innerHTML = mult
 document.getElementById("q4d").innerHTML = div
-
-
-
 
 // 5. Declare two variables with string values.
 //    Create a third variable that concatenates the two strings
@@ -86,19 +67,15 @@ var concatenated = `${string1} ${string2}`;
 
 document.getElementById("q5").innerHTML = concatenated;
 
-
-
-
-
 // 6. Add, subract, multiply and divide the strings '5' and '3'.
 //    Observe the results and brieftly explain the results within the HTML page.
 //
 var x = '5';
 var y = '3';
-var xPlusY = x+y;
-var xMinusY = x-y;
-var xTimesY = x*y;
-var xDividedY = x/y;
+var xPlusY = x + y;
+var xMinusY = x - y;
+var xTimesY = x * y;
+var xDividedY = x / y;
 
 document.getElementById("q6a").innerHTML = xPlusY;
 document.getElementById("q6b").innerHTML = xMinusY;
@@ -109,33 +86,24 @@ var note = "The strings were concatenated with the addition sign.  Otherwise, th
 
 document.getElementById("q6note").innerHTML = note;
 
-
-
 // 7. When a button is clicked, add content into the ID "q7".
 
 var woohoo = "It worked!";
 
-document.getElementById("callAlert").addEventListener("click", function () {
-  document.getElementById("q7").innerHTML = woohoo;
+document.getElementById("callAlert").addEventListener("click", function() {
+    document.getElementById("q7").innerHTML = woohoo;
 });
-
-
 
 // 8. Collect a number value from the user and when a button is clicked,
 //    then double user's answer and display that value into the HTML page.
 
 // Wait till a button is clicked
 document.getElementById("callDoubleNum").addEventListener("click", function() {
-
-  // Collect Data
-  var doubled = document.getElementById("doubleNum").value;
-
-  // Display Data * 2
-  document.getElementById("q8").innerHTML = (doubled*2);
-
+    // Collect Data
+    var doubled = document.getElementById("doubleNum").value;
+    // Display Data * 2
+    document.getElementById("q8").innerHTML = (doubled * 2);
 });
-
-
 
 // 9. Set the variable 'num1' to 5.
 //    Set the variable 'num2' to 10.
@@ -146,15 +114,13 @@ document.getElementById("callDoubleNum").addEventListener("click", function() {
 var num1 = 5;
 var num2 = 10;
 
-if (num1 > num2){
-var result = "Yes, the value of num1 is greater than num2";
+if (num1 > num2) {
+    var result = "Yes, the value of num1 is greater than num2";
 } else {
-var result = "No, the value of num1 is not greater than num2";
+    var result = "No, the value of num1 is not greater than num2";
 }
 
 document.getElementById("q9").innerHTML = result;
-
-
 
 // 10. Set the variable 'score' to a number between 1-100.
 //    Create the following conditions:
@@ -166,16 +132,14 @@ document.getElementById("q9").innerHTML = result;
 var score = 99;
 
 if (score > 90) {
-  var grade = "Great job";
+    var grade = "Great job";
 } else if (score > 60 && score <= 89) {
-  var grade = "Nice work, keep practicing";
+    var grade = "Nice work, keep practicing";
 } else if (score <= 59) {
-  var grade = "Very bad: study more and retake the test";
+    var grade = "Very bad: study more and retake the test";
 };
 
 document.getElementById("q10").innerHTML = grade;
-
-
 
 // ADVANCED TRACK
 
@@ -183,24 +147,21 @@ document.getElementById("q10").innerHTML = grade;
 //    In a single command, take the user's answers and display into the HTML page the sentence:
 //   "My name is __. I'm __ years old, and I live in __."
 
+document.getElementById("submit").addEventListener("click", function() {
 
-document.getElementById("submit").addEventListener("click", function () {
-
-  var aName = document.getElementById("aName").value;
-  var aAge  = document.getElementById("aAge").value;
-  var aCity = document.getElementById("aCity").value;
-  var qASentence = "My name is " + aName + "." + " I'm " + aAge + " years old, and I live in " + aCity + ".";
-  document.getElementById("qa").innerHTML = qASentence;
+    var aName = document.getElementById("aName").value;
+    var aAge = document.getElementById("aAge").value;
+    var aCity = document.getElementById("aCity").value;
+    var qASentence = "My name is " + aName + "." + " I'm " + aAge + " years old, and I live in " + aCity + ".";
+    document.getElementById("qa").innerHTML = qASentence;
 });
-
-
 
 // B. Create the formula to determine the area of a rectangle.
 //     The rectangle is: 2 inches by 3 inches
 //     Display the result in the HTML page.
-//
+
 var length = 2;
-var width  = 3;
+var width = 3;
 var rectangleArea = length * width;
 
 document.getElementById("qb").innerHTML = rectangleArea;
@@ -209,22 +170,18 @@ document.getElementById("qb").innerHTML = rectangleArea;
 // C. Collect the use's length and width input to determine the area of a rectangle and display within the HTML page the result.
 
 document.getElementById("cArea").addEventListener("click", function() {
-  var cWidth = document.getElementById("cWidth").value;
-  var cLength= document.getElementById("cLength").value;
-  var area  = (cWidth * cLength);
-})
-
-document.getElementById("qc").innerHTML = area;
-
-
+        var cWidth = document.getElementById("cWidth").value;
+        var cLength = document.getElementById("cLength").value;
+        var area = cWidth * cLength;
+        document.getElementById("qc").innerHTML = area;
+    })
 
 // D. Have the user enter their name into a textfield. When they click a button, find the length (number of characters) of their name and display the result in the HTML page.
 
-
-
-
-
-
+document.getElementById("dCharacters").addEventListener("click", function() {
+    var dLength = document.getElementById("dName").value;
+    document.getElementById("qd").innerHTML = dLength.length;
+});
 
 // E. Create a textfield to request a test score between 0-100 from the user.
 //    If the score is above 90, display in the HTML a message to tell the user "Great job!".
@@ -232,9 +189,18 @@ document.getElementById("qc").innerHTML = area;
 //    If the score is below 59, display a message of "Very bad: study more and retake the test.".
 //    You can get fancy by adding CSS styles to each of these displays to change the color, too - Green=Good, Orange=OK, Red=Bad
 
+document.getElementById("eResults").addEventListener("click", function() {
+      var results = document.getElementById('eScore').value;
+      if (results >= 90) {
+        document.getElementById('qe').innerHTML = 'Great job!'
+      } else if (results <= 90 && results >=60) {
+        document.getElementById('qe').innerHTML = "Nice work, keep practicing."
+      } else {
+        document.getElementById('qe').innerHTML = "Very bad: study more and retake the test."
+      }
+})
 
-
-
+//=============HOW WOULD YOU ADD STYLES TO EACH OUTCOME WHEN THE STILE DEPENDS ON THE OUTCOME?
 
 /* SANDBOX TRACK
 
